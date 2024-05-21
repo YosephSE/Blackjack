39f728c55842ec67af7965af37aca914076f1cc4
YosephSE <88291669+YosephSE@users.noreply.github.com>
2024-05-21 14:01:41 +0300

    minor canges

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 let player = {
-    name: "Per",
-    chips: 200
+    name: "Yoseph",
+    chips: 1000
 }
 
 let cards = []
@@ -12,11 +12,12 @@ let messageEl = document.getElementById("message-el")
 let sumEl = document.getElementById("sum-el")
 let cardsEl = document.getElementById("cards-el")
 let playerEl = document.getElementById("player-el")
+let newCardButton = document.querySelector("button:nth-of-type(2)")
 
 playerEl.textContent = player.name + ": $" + player.chips
 
 function getRandomCard() {
-    let randomNumber = Math.floor( Math.random()*13 ) + 1
+    let randomNumber = Math.floor( Math.random() * 13 ) + 1
     if (randomNumber > 10) {
         return 10
     } else if (randomNumber === 1) {
@@ -28,6 +29,7 @@ function getRandomCard() {
 
 function startGame() {
     isAlive = true
+    hasBlackJack = false
     let firstCard = getRandomCard()
     let secondCard = getRandomCard()
     cards = [firstCard, secondCard]
@@ -42,18 +44,24 @@ function renderGame() {
     }
     
     sumEl.textContent = "Sum: " + sum
+    messageEl.classList.remove("win", "lose") // Remove previous classes
+    
     if (sum <= 20) {
         message = "Do you want to draw a new card?"
+        newCardButton.disabled = false
     } else if (sum === 21) {
         message = "You've got Blackjack!"
         hasBlackJack = true
+        messageEl.classList.add("win") // Add win class
+        newCardButton.disabled = true
     } else {
         message = "You're out of the game!"
         isAlive = false
+        messageEl.classList.add("lose") // Add lose class
+        newCardButton.disabled = true
     }
     messageEl.textContent = message
 }
-
 
 function newCard() {
     if (isAlive === true && hasBlackJack === false) {
